07877c57b3b847749063b9540d48f56b9b1ced80
서재형 <woway2488@naver.com>
2023-12-17 22:50:54 +0900

    feat : 장비 출납 현황 조회 API 틀 작성

---
+++ trainning1team/src/main/java/com/bnksystem/trainning1team/mapper/EquipMapper.java
@@ -0,0 +1,9 @@
+package com.bnksystem.trainning1team.mapper;
+
+import com.bnksystem.trainning1team.dto.Equip.EquipResponse;
+
+import java.util.List;
+
+public interface EquipMapper {
+    List<EquipResponse> getTotalEquipList();
+}
---
+++ trainning1team/src/main/java/com/bnksystem/trainning1team/service/EquipService.java
@@ -0,0 +1,19 @@
+package com.bnksystem.trainning1team.service;
+
+import com.bnksystem.trainning1team.dto.Equip.EquipResponse;
+import com.bnksystem.trainning1team.mapper.EquipMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EquipService {
+
+    private final EquipMapper equipMapper;
+
+    public List<EquipResponse> getTotalEquipList() {
+        return equipMapper.getTotalEquipList();
+    }
+}
