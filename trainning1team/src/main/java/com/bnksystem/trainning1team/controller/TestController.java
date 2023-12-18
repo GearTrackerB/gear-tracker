793135f79e85ef8b685ccd716585578be1d00fe6
SeongHo Ha <tjdghgk98@gmail.com>
2023-12-18 16:34:24 +0900

    feat: jsp 페이지 로딩 테스트

--- trainning1team/src/main/java/com/bnksystem/trainning1team/controller/TestController.java
+++ trainning1team/src/main/java/com/bnksystem/trainning1team/controller/TestController.java
@@ -2,9 +2,10 @@ package com.bnksystem.trainning1team.controller;
 
 import com.bnksystem.trainning1team.service.TestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class TestController {
 
@@ -15,9 +16,15 @@ public class TestController {
     *   1. DB 접근
     *   2. 리턴 확인
     * */
+    @ResponseBody
     @GetMapping("/test")
     public String test(){
         testService.test();
         return "test 성공";
+    }
+
+    @GetMapping("/test-main")
+    public String testMain(){
+        return "main";
     }
 }
