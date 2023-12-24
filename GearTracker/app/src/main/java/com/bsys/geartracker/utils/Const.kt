d1b7ec04fb8d1beeaecea261172f5db80199993d
서재형 <woway2488@naver.com>
2023-12-24 23:49:34 +0900

    feat : server url aws로 변경

--- GearTracker/app/src/main/java/com/bsys/geartracker/utils/Const.kt
+++ GearTracker/app/src/main/java/com/bsys/geartracker/utils/Const.kt
@@ -1,8 +1,9 @@
 package com.bsys.geartracker.utils
 
 // 서버 연결 주소
+const val BASE_URL = "http://14.63.221.64:48083/"
 //const val BASE_URL = "http://43.200.7.145:8080/"
-const val BASE_URL = "http://192.168.0.95:8080/"
+//const val BASE_URL = "http://192.168.0.95:8080/"
 //const val BASE_URL = "http://192.168.0.138:8080/"
 
 
@@ -14,6 +15,7 @@ const val EQUIP_INVENTORY = 3
 const val EQUIP_DETAIL = 4
 const val EQUIP_TOTAL_INFO = 5
 const val EQUIP_INVETORY_INFO = 6
+const val FROM_DETAIL_INFO = 7
 
 // 장비 조회 아이템 Size
-const val LIST_SIZE = 20
+const val LIST_SIZE = 4
